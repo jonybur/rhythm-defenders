f7503e4babcced6b580734e54e302b8324a55316
Jonathan <jobur93@gmail.com>
2014-05-16 23:57:51 -0300

    Base del juego

---
+++ AudioGalaga/Assets/scripts/ParticleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+
+	void Start ()
+	{
+		//Change Foreground to the layer you want it to display on 
+		//You could prob. make a public variable for this
+		particleSystem.renderer.sortingLayerName = "stars";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AudioGalaga/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
+		{
+			AccionesNave(Input.GetMouseButton(0), Input.mousePosition);
+		}
+		else if (Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			AccionesNave(Input.touches.Length > 0, (Input.touches.Length > 0) ? (Vector3)Input.touches[0].position : new Vector3());
+		}
+	}
+
+	void AccionesNave (bool toca, Vector3 posicionTouch)
+	{
+		posicionTouch = Camera.main.ScreenToWorldPoint(posicionTouch);
+
+		if (toca)
+		{
+			this.transform.position = new Vector3 (posicionTouch.x, 
+			                                       posicionTouch.y + this.renderer.bounds.size.y,
+			                                       0);
+		}
+	}
+}
