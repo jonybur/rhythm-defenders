b031bf2f43cdc938eb0439c8c100896be7605dfc
Jonathan <jobur93@gmail.com>
2014-06-01 01:54:56 -0300

    Agregado botón de disparo presionado-no presionado y su PSD correspondiente

--- Unity/Assets/scripts/ControladorTouch.cs
+++ Unity/Assets/scripts/ControladorTouch.cs
@@ -10,15 +10,23 @@ public class ControladorTouch : MonoBehaviour {
 	float rate;
 	float index;
 
+	public Sprite[] botones;
+	SpriteRenderer rendererBoton;
+
+	bool butPres;
 	
 	void Awake()
 	{			
+		rendererBoton = (SpriteRenderer)GameObject.Find("boton_disparo").GetComponent("SpriteRenderer");
 		index = 0f;
 		rate = 0.3f;
+		butPres = false;
 	}
 	
 	void Update () {
 		index += Time.deltaTime;
+		butPres = false;
+
 		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
 		{
 			/* TODO: BORRAR LA PARTE DE DEBUG EN PC */
@@ -35,15 +43,29 @@ public class ControladorTouch : MonoBehaviour {
 				}
 			}
 		}
+
+		if (butPres)
+		{
+			rendererBoton.sprite = botones[1];
+		}else{
+			
+			rendererBoton.sprite = botones[0];
+		}
+			
 	}
 
 	void Acciones(RaycastHit hit, Vector3 posicion)
 	{
-		if (hit.transform.gameObject.name == "boton_disparo" && index > rate)
+		if (hit.transform.gameObject.name == "boton_disparo")
 		{
-			GameObject b = (GameObject)Instantiate(bala);
-			b.transform.position = nave.transform.position + new Vector3(0,nave.renderer.bounds.size.y/2,0);
-			index = 0; 
+			butPres = true;
+			if (index > rate)
+			{
+
+				GameObject b = (GameObject)Instantiate(bala);
+				b.transform.position = nave.transform.position + new Vector3(0,nave.renderer.bounds.size.y/2,0);
+				index = 0; 
+			}
 		}
 
 		else if (hit.transform.gameObject.name == "control_nave")
