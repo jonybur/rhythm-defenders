45885071ee7a6d61f447a0a71bb10bf824c4973d
Jonathan Bursztyn <jobur93@gmail.com>
2014-07-19 00:14:45 -0300

    Agregado efecto de velocidad variable a las estrellas. Modificado un poco el orden de la jerarquía.

--- Unity/Assets/scripts/particulas/ParticleScript.cs
+++ Unity/Assets/scripts/particulas/ParticleScript.cs
@@ -3,8 +3,26 @@ using System.Collections;
 
 public class ParticleScript : MonoBehaviour {
 
-	void Start ()
-	{
-		particleSystem.renderer.sortingLayerName = "stars";
-	}
+    GetSpectrumScript a;
+    float playbackSpeed;
+
+    void Awake()
+    {
+        this.particleSystem.renderer.sortingLayerName = "stars";
+
+        try
+        {
+            a = (GetSpectrumScript)GameObject.Find("audio").GetComponent("GetSpectrumScript");
+        }
+        catch
+        {
+            a = (GetSpectrumScript)GameObject.Find("audio_debug").GetComponent("GetSpectrumScript");
+        }
+        playbackSpeed = this.particleSystem.playbackSpeed;
+    }
+
+    void Update()
+    {
+        this.particleSystem.playbackSpeed = playbackSpeed + a.curValues[0];
+    }
 }
